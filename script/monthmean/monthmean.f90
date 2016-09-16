dcf3d4f6063ac0ce454c7de7235c908ed6c5dba4
irudeva <irinarudeva@gmail.com>
2016-09-16 14:23:34 +1000

    plot n70 v1

--- script/monthmean/monthmean.f90
+++ script/monthmean/monthmean.f90
@@ -3,12 +3,13 @@ Program pdf
 character*100 ftrk,fout
 integer yr,nreg
 
-
-integer,allocatable:: cyr(:),m(:)
+integer,allocatable:: cyr(:),m(:),num(:),loc(:)
 real, allocatable:: x(:),y(:),p(:),c(:),dp(:),d(:),rd(:),rr(:)
-real mp,md,mdp,mrd,sigp,sigrd,sigd,sigdp,mc,sigc,mrr,sigrr
+real mp,md,mdp,mrd,sigp,sigrd,sigd,sigdp,mc,sigc,mrr,sigrr,mn70,mn70cont,sign70,sign70cont
 
 integer i,nc,n,nm,ndm(12)
+integer ntrk, len,len70,maxlen
+integer, allocatable,dimension(:)::n70,n70cont
 
 read (*,'(a)') ftrk
 read (*,*) yr
@@ -29,20 +30,23 @@ nyr=20
 endif
 
 open(30,file=fout)
-write(30,'("mmyyyy ncyc ncyc/an",5(7x,a,7x,"sig"),4(6x,a))')"  P","Dsq"," dp","  R"," RR","IDsq","  ID","  IR"," IRR"
+write(30,'("mmyyyy ncyc ncyc/an"7(6x,a,7x,"sig"),4(6x,a))')"   P"," Dsq","  dp","   R","  RR"," n70","n70c","IDsq","  ID","  IR"," IRR"
 
 do 4 nm=1,12
 open (10,file=ftrk,action='read')
 open (20,file="dummy")
+open (21,file="dummy1")
 
 do 1 i=1,66
 1 read(10,*)
 
 
 nc=0
+ntrk=0
 do
+ ptrk=0
  read(10,'(55x,i4)',end=22) n
- allocate (cyr(n),m(n),x(n),y(n),p(n),c(n),dp(n),rd(n),d(n))
+ allocate (cyr(n),m(n),x(n),y(n),p(n),c(n),dp(n),rd(n),d(n),loc(n))
  read(10,*)
  read(10,*)
  do 21 i=1,n
@@ -52,29 +56,69 @@ do
  if (cyr(i)==nyr.and.n>=5.and.y(i)>=70..and.m(i)==nm)then
  if (nreg==0.or.(nreg==1.and.(x(i)<-135..or.x(i)>90.)).or.&
 &(nreg==2.and.(x(i)<-20..or.x(i)>160.)).or.(nreg==3.and.x(i)<-80.).or.(nreg==4.and.(x(i)<-110.)))then
- nc=nc+1
-  write(20,1002) nc,i,n, p(i),c(i),dp(i),rd(i)
-1002 format(i7,2i4,5f10.2)
+  nc=nc+1
+  if (ptrk==0)then
+   ntrk=ntrk+1
+   ptrk=1
+  end if
+  write(20,1002) nc,ntrk,i,n, p(i),c(i),dp(i),rd(i)
+1002 format(2i7,2i4,4f10.2)
  endif
- endif	
+ endif
  21 continue
 
+ ! write length
+ loc=0
+ if(ptrk==1)then
+  do 23 i=1,n
+   if (cyr(i)==nyr.and.y(i)>=70.)then
+   if (nreg==0.or.(nreg==1.and.(x(i)<-135..or.x(i)>90.)).or.&
+&(nreg==2.and.(x(i)<-20..or.x(i)>160.)).or.(nreg==3.and.x(i)<-80.).or.(nreg==4.and.(x(i)<-110.)))then
+    loc(i)=1
+   end if
+   end if
+  23 continue
+  len70=count(loc==1)
+  len=0
+  maxlen=0
+  do 24 i=1,n
+   if(loc(i)==1)then
+    len=len+1
+   else
+    if (len>=maxlen)maxlen=len
+    len=0
+   end if
+   if(i==n) maxlen=maxval((/maxlen,len/) )
+  24 end do
+  write(21,1005) ntrk,n,len70,maxlen
+
+  1005 format(4i7)
+ end if
+
  read(10,*)
- deallocate (cyr,x,y,p,c,dp,rd,d,m)
+ deallocate (cyr,x,y,p,c,dp,rd,d,m,loc)
 
 end do
 
 22 close(10)
-rewind(20);print*, nm,yr,nc
+rewind(20);rewind(21);print*, nm,yr,nc
 
-allocate (p(nc),dp(nc),d(nc),rd(nc),c(nc),rr(nc))
+allocate (num(nc),p(nc),dp(nc),d(nc),rd(nc),c(nc),rr(nc))
 
 if (nc>1)then
 do 3 i=1,nc
-read (20,1004) p(i),c(i),dp(i),rd(i)
+read (20,1004) num(i),p(i),c(i),dp(i),rd(i)
 3 rr(i)=rd(i)**2
-1004 format (15x,5f10.2)
+1004 format (7x,i7,8x,5f10.2)
 close(20,status='delete')
+!close(20)
+
+allocate(n70(ntrk),n70cont(ntrk))
+do 31 i=1,ntrk
+31 read (21,'(14x,2i7)') n70(i),n70cont(i)
+close(21,status='delete')
+!close(21)
+
 
 call moment(p,nc,mp,sigp)
 !call moment(d,nc,md,sigd)
@@ -82,12 +126,18 @@ call moment(dp,nc,mdp,sigdp)
 call moment(rd,nc,mrd,sigrd)
 call moment(c,nc,mc,sigc)
 call moment(rr,nc,mrr,sigrr)
+call moment(float(n70),ntrk,mn70,sign70)
+call moment(float(n70cont),ntrk,mn70cont,sign70cont)
 
-write(30,1003)nm, yr,nc,float(nc)/ndm(nm),mp,sigp,mc,sigc,mdp,sigdp,mrd,sigrd,mrr,sigrr,sum(c),sum(dp),sum(rd),sum(rr)
+
+write(30,1003)nm, yr,nc,float(nc)/ndm(nm),mp,sigp,mc,sigc,mdp,sigdp,mrd,sigrd,mrr,sigrr,mn70,sign70,mn70cont,sign70cont,sum(c),&
+& sum(dp),sum(rd),sum(rr)
 endif  ! nc>1
-4 deallocate(p,d,dp,rd,c,rr)
+4 deallocate(num,p,d,dp,rd,c,rr,n70,n70cont)
 close(30)
-1003 format(i2,i4,i5,f8.2,16f10.2)
+1003 format(i2,i4,i5,f8.2,20f10.2)
+
+stop
 
 end
 
@@ -98,7 +148,7 @@ end
       INTEGER j
       REAL p,s,ep
       if(n.le.1)then
-       Write(*,'( "n must be at least 2 in moment")') 
+       Write(*,'( "n must be at least 2 in moment")')
        read(*,*)
       endif
       s=0.
@@ -134,4 +184,3 @@ end
       endif
       return
       END
-
--- script/monthmean/monthmean.sh
+++ script/monthmean/monthmean.sh
@@ -3,64 +3,64 @@ prog=monthmean
 gfortran -o $prog.exe $prog.f90
 
 sy=1979
-ly=2011
+ly=2016
 HS="N"
-#mon=( "Jan" "Feb" "Mar" "Apr" "May" "Jun" "Jul" "Aug" "Sep" "Oct" "Nov" "Dec" ) 
+d="ncep"
+lev="slp"
+#mon=( "Jan" "Feb" "Mar" "Apr" "May" "Jun" "Jul" "Aug" "Sep" "Oct" "Nov" "Dec" )
 
-Din=/work/irudeva/tracking/UOM/MERRA/out/merra.
-Dout=/work/irudeva/Arctic/MERRA/monthmean/
+Din=~/work/Projects/Arctic/DATA/NCEP/trk/
+Dout=~/work/Projects/Arctic/output/trkmonmean/
 reg=( "" ".90_225" ".160_340" ".180_280" ".180_250" )
 echo $reg
 #reg=.BeaufortS
-nreg=3
-lreg=3
+nreg=0
+lreg=0
 while [ $nreg -le $lreg ]
 do
 
-fout=${Dout}av.$sy-$ly$HS${reg[$nreg]}.dat
-fssn=${Dout}cycstat.jja$sy-$ly$HS${reg[$nreg]}.dat
-fmay=${Dout}cycstat.may$sy-$ly$HS${reg[$nreg]}.dat
-fjun=${Dout}cycstat.jun$sy-$ly$HS${reg[$nreg]}.dat
-fjul=${Dout}cycstat.jul$sy-$ly$HS${reg[$nreg]}.dat
-faug=${Dout}cycstat.aug$sy-$ly$HS${reg[$nreg]}.dat
-fsep=${Dout}cycstat.sep$sy-$ly$HS${reg[$nreg]}.dat
-
-
+fout=${Dout}av.${lev}.$d.$sy-$ly$HS${reg[$nreg]}.dat
+fssn=${Dout}cycstat.${lev}.$d.jja$sy-$ly$HS${reg[$nreg]}.dat
+fmay=${Dout}cycstat.${lev}.$d.may$sy-$ly$HS${reg[$nreg]}.dat
+fjun=${Dout}cycstat.${lev}.$d.jun$sy-$ly$HS${reg[$nreg]}.dat
+fjul=${Dout}cycstat.${lev}.$d.jul$sy-$ly$HS${reg[$nreg]}.dat
+faug=${Dout}cycstat.${lev}.$d.aug$sy-$ly$HS${reg[$nreg]}.dat
+fsep=${Dout}cycstat.${lev}.$d.sep$sy-$ly$HS${reg[$nreg]}.dat
 
 y=$sy
 while [ $y -le $ly ]
 do
 
-fin=$Din$y$HS.trk
-fouty=${Dout}av.$y$HS${reg[$nreg]}
+fin=${Din}trkdat.${lev}.$d.$y
+fouty=${Dout}av.${lev}.$d.$y$HS${reg[$nreg]}
 
-#./$prog.exe<<mark
-#$fin
-#$y
-#$fouty
-#$nreg
-#mark
+./$prog.exe<<mark
+$fin
+$y
+$fouty
+$nreg
+mark
 
 y=$[$y+1]
 done
 
-gfortran -o av.exe av_m.f90
-./av.exe<<mark
-$Dout
-$HS
-$fout
-$fssn
-$fmay
-$fjun
-$fjul
-$faug
-$fsep
-$sy
-$ly
-$nreg
-mark
-rm av.exe
-#rm $Dout????$HS
+#gfortran -o av.exe av_m.f90
+#./av.exe<<mark
+# #$Dout
+# $HS
+# $fout
+# $fssn
+# $fmay
+# $fjun
+# $fjul
+# $faug
+# $fsep
+# $sy
+# $ly
+# $nreg
+# mark
+# rm av.exe
+# #rm $Dout????$HS
 
 nreg=$[$nreg+1]
 done
